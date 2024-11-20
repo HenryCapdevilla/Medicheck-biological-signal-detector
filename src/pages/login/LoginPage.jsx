0a8cb04fa5908bc952ac16e599eecb51bf9ff9b5
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-20 03:15:53 -0500

    Modificación del login forms

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -9,7 +9,7 @@ function LoginPage() {
     const { signin, errors: loginErrors } = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
-
+    
     const onSubmit = async (data) => {
         signin(data); // Captura el resultado
         const redirectPath = location.state?.from || '/'; // Ruta a donde redirigir después del login
@@ -17,43 +17,38 @@ function LoginPage() {
     };
 
     return (
-        <div className="container d-flex justify-content-center align-items-center min-vh-100">
-            <form 
-                className="bg-light shadow-lg rounded p-4 w-100 w-md-50"
-                onSubmit={handleSubmit(onSubmit)}
-            >
+        <div className="register-container">
+            <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
                 <Character />
                 {loginErrors.map((error, i) => (
-                    <div className="alert alert-danger" role="alert" key={i}>
+                    <div className="Error-1" key={i}>
                         {error}
                     </div>
                 ))}
-                
-                <div className="mb-3">
-                    <input 
-                        type="email" 
-                        className="form-control" 
-                        {...register("email", { required: true })} 
-                        placeholder="Email" 
+                <div className="form-group mb-3">
+                    <input
+                        type="email"
+                        {...register("email", { required: true })}
+                        placeholder="Email"
+                        className="form-control form-control-sm w-auto"
                     />
-                    {errors.email && <p className="text-danger">Email is required</p>}
+                    {errors.email && <p className="Error">Email is required</p>}
                 </div>
 
-                <div className="mb-3">
-                    <input 
-                        type="password" 
-                        className="form-control" 
-                        {...register("password", { required: true })} 
-                        placeholder="Password" 
+                <div className="form-group mb-3">
+                    <input
+                        type="password"
+                        {...register("password", { required: true })}
+                        placeholder="Password"
+                        className="form-control form-control-sm w-auto"
                     />
-                    {errors.password && <p className="text-danger">Password is required</p>}
+                    {errors.password && <p className="Error">Password is required</p>}
                 </div>
 
-                <Button text="Login" className="btn btn-primary w-100 mb-3" />
+                <Button text="Login" />
 
-                <p className="text-center">
-                    Don't have an account?&nbsp;
-                    <Link to="/register">Register</Link>
+                <p className="redirect-link">
+                    Don't have an account?&nbsp;<Link to="/register">Register</Link>
                 </p>
             </form>
         </div>
