416cb67ecb22ffeb4d7abe2386ca44b21437a19b
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-20 01:52:49 -0500

    cookies :c

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,11 +2,16 @@ import axios from './axios.js';
 
 export const registerRequest = user => axios.post(`/register`, user);
 
-export const loginRequest = user => axios.post(`/login`, user)
+export const loginRequest = user => axios.post(`/login`, user);
 
-export const verifyTokenRequest = user => axios.get(`/verify`, user)
+export const verifyTokenRequest = token => axios.get(`/verify`, {
+    headers: {
+        Authorization: `Bearer ${token}`,
+    }
+});
 
-export const logoutRequest = user => axios.post("/logout", user)
+
+export const logoutRequest = user => axios.post("/logout", user);
 
 // Funciones para manejar la historia clínica
 export const upClinicalHRequest = data => axios.post(`/clinical-history`, data);
