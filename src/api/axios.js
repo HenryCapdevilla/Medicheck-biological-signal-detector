e290b0f34d01f9efd47f4198540b6246ad1d9685
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-19 19:29:57 -0500

    Prueba-6_Axios_Router_baseurl modificado

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://medicheck.website/auth',
+    baseURL: 'https://medicheck.website',
     withCredentials: true // Permitir el envío de cookies
 });
 
