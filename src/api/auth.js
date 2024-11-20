53111d9a0eea7edcdf9c675cccfade23c35d6e8a
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-19 19:33:40 -0500

    Prueba-7

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import axios from './axios.js';
 
 export const registerRequest = user => axios.post(`/register`, user);
 
-export const loginRequest = user => axios.post(`/login`, user, { withCredentials: true })
+export const loginRequest = user => axios.post(`/login`, user)
 
 export const verifyTokenRequest = user => axios.get(`/verify`, user)
 
