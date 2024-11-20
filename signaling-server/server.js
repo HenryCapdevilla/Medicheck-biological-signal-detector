cd539436488b87fe8ae8952c5a27588d5df0f527
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-20 00:13:44 -0500

    Más pruebas de server signal - anterior no funciono :c

--- signaling-server/server.js
+++ signaling-server/server.js
@@ -6,11 +6,9 @@ const io = require("socket.io")(server, {
     cors: {
         origin: "*",  // Permite cualquier origen
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,  // Mantiene la opción para las credenciales (cookies, autenticación)
     }
 });
-
 
 let rooms = {}; // Almacena los RoomID y los usuarios asociados a cada uno
 
