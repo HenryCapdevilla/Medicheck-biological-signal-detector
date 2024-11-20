897b8230933796643f20eb7526d2f277128df25b
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-20 01:06:56 -0500

    intento 3? problema de auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,162 +1,153 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { registerRequest, loginRequest, verifyTokenRequest, logoutRequest, upClinicalHRequest, downClinicalHRequest, updateClinicalHRequest } from '../api/auth.js';
+import { 
+  registerRequest, 
+  loginRequest, 
+  verifyTokenRequest, 
+  logoutRequest, 
+  upClinicalHRequest, 
+  downClinicalHRequest, 
+  updateClinicalHRequest 
+} from '../api/auth.js';
 import Cookies from 'js-cookie';
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export const useAuth = () =>{
+export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
-        throw new Error("useAuth must be used within an AuthProvider")
+        throw new Error("useAuth must be used within an AuthProvider");
     }
-    return context
-}
-
-export const AuthProvider = ({ children }) => {
-  // Cambia "Children" a "children"
-  const [user, setUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [errors, setErrors] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const signup = async (user) => {
-    try {
-      const res = await registerRequest(user);
-      console.log(res.data);
-      setUser(res.data);
-      setIsAuthenticated(true);
-    } catch (error) {
-      console.log(error.response.data);
-        if (Array.isArray(error.response.data)) {
-            console.log(error.response.data)
-            setErrors(error.response.data);
-        } else {
-            setErrors([error.response.data]); // Asegúrate de que sea un array
-        }
-    }
-  };
-
-  const signin = async (user) => {
-    try {
-        const res = await loginRequest(user);
-        console.log(res);
-        setIsAuthenticated(true);
-        setUser(res.data);
-        return true; // Retorna true para indicar que el inicio de sesión fue exitoso
-    } catch (error) {
-        if (Array.isArray(error.response.data)) {
-            setErrors(error.response.data);
-        } else {
-            setErrors([error.response.data]); // Asegúrate de que sea un array
-        }
-        return false; // Retorna false para indicar que hubo un error
-    }
+    return context;
 };
 
-  // Función para cerrar sesión
-  const logout = async () => {
-    try {
-      const res = await logoutRequest();
-      if (res.status === 200) {
-        // Eliminar el token de las cookies
-        Cookies.remove("token");
-        setUser(null);
-        setIsAuthenticated(false);
-        console.log("Cierre de sesión exitoso");
-      } else {
-        console.log("Hubo un problema al cerrar sesión");
-      }
-    } catch (error) {
-      console.log("Error al cerrar sesión:", error);
-    }
-  };
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    // Subir nueva historia clínica
-    const handleUploadHistory = async (data) => {
-      try {
-          const res = await upClinicalHRequest(data);
-          console.log("Historia clínica subida:", res.data);
-      } catch (error) {
-          console.error("Error al subir historia clínica:", error);
-      }
-  };
-
-  // Descargar historia clínica
-  const handleDownloadHistory = async () => {
-      try {
-          const response = await downClinicalHRequest();
-          console.log("Datos de la historia clínica:", response.data);
-      } catch (error) {
-          console.error("Error al descargar historia clínica:", error);
-      }
-  };
-
-  // Actualizar historia clínica
-  const handleUpdateHistory = async (data) => {
-      try {
-          const response = await updateClinicalHRequest(data);
-          console.log("Historia clínica actualizada:", response.data);
-      } catch (error) {
-          console.error("Error al actualizar historia clínica:", error);
-      }
-  };
-
-  //Para eliminar los errores mostrados luego de 5 segundos
-  useEffect(() => {
-    if (errors.length > 0) {
-      const timer = setTimeout(() => {
-        setErrors([]);
-      }, 8000);
-      return () => clearTimeout(timer);
-    }
-  }, [errors]);
-
-  useEffect(() => {
-    const checkLogin = async () => {
-        const token = Cookies.get("token");
-        if (!token) {
-            setUser(null);
-            setIsAuthenticated(false);
-            setLoading(false);
-            return;
-        }
+    const signup = async (user) => {
         try {
-            const res = await verifyTokenRequest(token);
-            if (res.data) {
-                setUser(res.data);
-                setIsAuthenticated(true);
-            } else {
-                setUser(null);
-                setIsAuthenticated(false);
-            }
+            const res = await registerRequest(user);
+            console.log(res.data);
+            setUser(res.data);
+            setIsAuthenticated(true);
         } catch (error) {
-            console.error("Error verifying token:", error);
-            setUser(null);
-            setIsAuthenticated(false);
-        } finally {
-            setLoading(false);
+            console.log(error.response?.data);
+            setErrors(Array.isArray(error.response?.data) ? error.response.data : [error.response?.data || "Unexpected error occurred"]);
         }
     };
-    checkLogin();
-}, []);
 
+    const signin = async (user) => {
+        try {
+            const res = await loginRequest(user);
+            console.log(res);
+            setIsAuthenticated(true);
+            setUser(res.data);
+            return true;
+        } catch (error) {
+            setErrors(Array.isArray(error.response?.data) ? error.response.data : [error.response?.data || "Unexpected error occurred"]);
+            return false;
+        }
+    };
 
-  return (
-    <AuthContext.Provider
-      value={{
-        signup,
-        signin,
-        logout,
-        handleUploadHistory,
-        handleDownloadHistory,
-        handleUpdateHistory,
-        loading,
-        user,
-        isAuthenticated,
-        errors,
-      }}
-    >
-      {children} {/* Cambia "Children" a "children" */}
-    </AuthContext.Provider>
-  );
+    const logout = async () => {
+        try {
+            const res = await logoutRequest();
+            if (res.status === 200) {
+                Cookies.remove("token");
+                setUser(null);
+                setIsAuthenticated(false);
+                console.log("Cierre de sesión exitoso");
+            } else {
+                console.log("Hubo un problema al cerrar sesión");
+            }
+        } catch (error) {
+            console.log("Error al cerrar sesión:", error);
+        }
+    };
+
+    const handleUploadHistory = async (data) => {
+        try {
+            const res = await upClinicalHRequest(data);
+            console.log("Historia clínica subida:", res.data);
+        } catch (error) {
+            console.error("Error al subir historia clínica:", error);
+        }
+    };
+
+    const handleDownloadHistory = async () => {
+        try {
+            const response = await downClinicalHRequest();
+            console.log("Datos de la historia clínica:", response.data);
+        } catch (error) {
+            console.error("Error al descargar historia clínica:", error);
+        }
+    };
+
+    const handleUpdateHistory = async (data) => {
+        try {
+            const response = await updateClinicalHRequest(data);
+            console.log("Historia clínica actualizada:", response.data);
+        } catch (error) {
+            console.error("Error al actualizar historia clínica:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (errors.length > 0) {
+            const timer = setTimeout(() => {
+                setErrors([]);
+            }, 8000);
+            return () => clearTimeout(timer);
+        }
+    }, [errors]);
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            const token = Cookies.get("token");
+            if (!token) {
+                setUser(null);
+                setIsAuthenticated(false);
+                setLoading(false);
+                return;
+            }
+            try {
+                const res = await verifyTokenRequest(token);
+                if (res.data) {
+                    setUser(res.data);
+                    setIsAuthenticated(true);
+                } else {
+                    setUser(null);
+                    setIsAuthenticated(false);
+                }
+            } catch (error) {
+                console.error("Error verifying token:", error);
+                setUser(null);
+                setIsAuthenticated(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkLogin();
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+                signup,
+                signin,
+                logout,
+                handleUploadHistory,
+                handleDownloadHistory,
+                handleUpdateHistory,
+                loading,
+                user,
+                isAuthenticated,
+                errors,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
