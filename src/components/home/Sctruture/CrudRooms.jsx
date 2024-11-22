6fe21d10b548a8b80024ec14a4b1eb65c5cfff6c
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-22 18:31:04 -0500

    correciones

--- src/components/home/Sctruture/CrudRooms.jsx
+++ src/components/home/Sctruture/CrudRooms.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BannerText from './BannerTextdefault.jsx';
 import { Container, Row, Col } from 'react-bootstrap';
 import "./divbanner.css"
-import DisplayButtonsdefault from "./BannerTextdefault"
+import DisplayButtonsdefault from "./displayButtonsdefault"
 
 const CrudRooms = () => {
   return (
