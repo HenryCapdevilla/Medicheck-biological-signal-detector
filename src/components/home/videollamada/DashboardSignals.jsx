31e4b04fec000dd13342530acddb08ceec439aef
HenryCapdevilla <henrydaniel2002@hotmail.com>
2024-11-22 20:14:25 -0500

    Correción función de ingresar de la videollamada #5

--- src/components/home/videollamada/DashboardSignals.jsx
+++ src/components/home/videollamada/DashboardSignals.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { HeartIcon, GlobeAltIcon } from '@heroicons/react/outline'; // Íconos para BPM y SPO2
-import { Card, Row, Col, Container } from 'react-bootstrap'; // Componentes de Bootstrap
 import './DashboardSignals.css';
 
 const UserCard = ({ heartRate, spO2 }) => {
@@ -11,32 +10,23 @@ const UserCard = ({ heartRate, spO2 }) => {
   };
 
   return (
-    <Container className="py-3">
-      <Row className="justify-content-center">
-        {/* Card de Frecuencia Cardiaca y SPO2 */}
-        <Col xs={12} sm={6} md={6} lg={6} className="mb-3 d-flex justify-content-center">
-          <Card className="statBox custom-card" onClick={handleClick}>
-            <Card.Body>
-              <HeartIcon className="icon mb-2" />
-              <Card.Text className={`statText ${clicked ? 'show' : ''}`}>
-                {heartRate} bpm
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+    <div className="user-card-container">
+      {/* Card de Frecuencia Cardiaca */}
+      <div className="card-container" onClick={handleClick}>
+        <div className="card-content">
+          <HeartIcon className="icon mb-2" />
+          <p className={`statText ${clicked ? 'show' : ''}`}>{heartRate} bpm</p>
+        </div>
+      </div>
 
-        <Col xs={12} sm={6} md={6} lg={6} className="mb-3 d-flex justify-content-center">
-          <Card className="statBox custom-card" onClick={handleClick}>
-            <Card.Body>
-              <GlobeAltIcon className="icon mb-2" />
-              <Card.Text className={`statText ${clicked ? 'show' : ''}`}>
-                {spO2}% O₂
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      {/* Card de SPO2 */}
+      <div className="card-container" onClick={handleClick}>
+        <div className="card-content">
+          <GlobeAltIcon className="icon mb-2" />
+          <p className={`statText ${clicked ? 'show' : ''}`}>{spO2}% O₂</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
